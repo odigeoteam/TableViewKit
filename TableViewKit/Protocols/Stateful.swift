4c6bc462853e61d83bf82544fe3eaa96fe66e4d8
Alfredo Delli Bovi <alfredo.dellibovi@gmail.com>
2016-09-26 17:45:04 +0200

    Add documentation for Stateful

--- TableViewKit/Protocols/Stateful.swift
+++ TableViewKit/Protocols/Stateful.swift
@@ -8,12 +8,33 @@
 
 import Foundation
 
+/// A section that supports states.
+///
+/// A stateful section keep the `currentState`
+/// and has a default implementation for `transition(to:)`
+/// needed to perform a transition from the `currentState` to a `newState`.
+/// You must implement `items(for:)`, that will be used to know
+/// which items belong to which `State`.
 public protocol Stateful: Section {
+    
+    /// A type that represent a state, such as an enum.
     associatedtype State
     
+    /// The current state
     var currentState: State { get set }
     
+    
+    /// Returns the `items` belonging to a `state`
+    ///
+    /// - parameter state: A concrete `state`
+    ///
+    /// - returns: The `items` belonging to a `state`
     func items(for state: State) -> [Item]
+    
+    
+    /// Performs a transition from the `currentState` to a `newState`
+    ///
+    /// - parameter newState: The `newState`
     func transition(to newState: State)
 }
 
