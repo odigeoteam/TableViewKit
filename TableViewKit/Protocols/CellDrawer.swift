edc05061faff672f4630f929382852327de265af
Nelson Domínguez <nelson.dominguez@odigeo.com>
2016-09-28 13:24:54 +0200

    Disable cell selection when item not implement Selectable protocol

--- TableViewKit/Protocols/CellDrawer.swift
+++ TableViewKit/Protocols/CellDrawer.swift
@@ -33,6 +33,7 @@ public extension CellDrawer {
     static func cell(in manager: TableViewManager, with item: Item, for indexPath: IndexPath) -> UITableViewCell {
         
         let cell = manager.tableView.dequeueReusableCell(withIdentifier: self.type.reusableIdentifier, for: indexPath)
+        cell.selectionStyle = item is Selectable ? .default : .none
         
         if let cell = cell as? ItemCompatible {
             cell.item = item
