f836bc5d5bc372937dd6760609f43d6eb08bfc5d
Alfredo Delli Bovi <alfredo.dellibovi@gmail.com>
2016-11-02 12:27:01 +0100

    Add docs for Editable

--- TableViewKit/Protocols/Editable.swift
+++ TableViewKit/Protocols/Editable.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+/// A type that represent an item that can be edited
 public protocol Editable: Item {
+    
+    /// The associated actions
     var actions: [UITableViewRowAction]? { get set }
 }
