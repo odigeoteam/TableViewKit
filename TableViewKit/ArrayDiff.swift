852354429cc37f9c41f49653bf97b85dd14fafcc
Alfredo Delli Bovi <alfredo.dellibovi@gmail.com>
2018-01-17 19:48:11 +0100

    Improve initial slicing

--- TableViewKit/ArrayDiff.swift
+++ TableViewKit/ArrayDiff.swift
@@ -36,6 +36,67 @@ enum ArrayIndexesChanges {
     case endUpdates
 }
 
+struct Movement: Hashable {
+    var hashValue: Int { return from.hashValue ^ to.hashValue }
+
+    static func == (lhs: Movement, rhs: Movement) -> Bool {
+        return (lhs.from == rhs.from && lhs.to == rhs.to)
+    }
+
+    let from: Int
+    let to: Int
+}
+
+extension Array where Element == ArrayIndexesChanges {
+    func filterInsertsIndexes(withOffset offset: Int) -> [Int] {
+        return flatMap { change -> Int? in
+            guard case .inserts(let index) = change else { return nil }
+            return index + offset
+        }
+    }
+
+    func filterDeletesIndexes(withOffset offset: Int) -> [Int] {
+        return flatMap { change -> Int? in
+            guard case .deletes(let index) = change else { return nil }
+            return index + offset
+        }
+    }
+
+    func filterMovesIndexes<Element>(insertsIndexes: inout [Int],
+                                     deletesIndexes: inout [Int],
+                                     between x: ArraySlice<Element>,
+                                     and y: ArraySlice<Element>,
+                                     where predicate: (Element, Element) -> Bool) -> [(Int, Int)] {
+        var moves = Set<Movement>()
+        let rangeX = (x.startIndex..<x.endIndex)
+        let rangeY = (y.startIndex..<y.endIndex)
+
+        var deleted: Int = 0
+        for (deleteIndex, deleteAtIndex) in deletesIndexes.enumerated() {
+            for (insertIndex, insertAtIndex) in insertsIndexes.enumerated() {
+                if rangeX.contains(deleteAtIndex) &&
+                    rangeY.contains(insertAtIndex) &&
+                    predicate(x[deleteAtIndex], y[insertAtIndex]) {
+                    deletesIndexes.remove(at: deleteIndex - deleted)
+                    insertsIndexes.remove(at: insertIndex)
+                    let movement: Movement
+                    if deleteAtIndex < insertAtIndex {
+                        movement = Movement(from: deleteAtIndex, to: insertAtIndex)
+                    } else {
+                        movement = Movement(from: insertAtIndex, to: deleteAtIndex)
+                    }
+                    moves.insert(movement)
+
+                    deleted += 1
+                    break
+                }
+            }
+        }
+
+        return moves.map { ($0.from, $0.to) }
+    }
+}
+
 class DiffIterator: IteratorProtocol {
     struct Coordinates {
         var x: Int
@@ -113,14 +174,85 @@ extension Array {
     static func diff(between x: [Element], and y: [Element],
                      subrange: Range<Index>? = nil,
                      where predicate: Predicate) -> Diff<Element> {
-        let subarray: [Element] = {
-            guard let subrange = subrange else { return x }
-            return Array(x[subrange.lowerBound..<subrange.upperBound])
-        }()
-        let lowerBond: Int = subrange?.lowerBound ?? 0
+        let sliceOfX: ArraySlice<Element>
+        let sliceOfY: ArraySlice<Element>
+        if let subrange = subrange {
+            sliceOfX = x[subrange]
+            sliceOfY = y[y.startIndex..<y.endIndex]
+        } else if x.startIndex == y.startIndex && x.endIndex == y.endIndex {
+            sliceOfX = makeOptimalSlice(between: x, and: y, where: predicate)
+            sliceOfY = y[sliceOfX.startIndex..<sliceOfX.endIndex]
+        } else {
+            sliceOfX = x[x.startIndex..<x.endIndex]
+            sliceOfY = y[y.startIndex..<y.endIndex]
+        }
 
-        var matrix = Matrix(rows: subarray.count + 1, columns: y.count + 1, repeatedValue: 0)
-        for (i, xElem) in subarray.enumerated() {
+        let matrix = makeLCSMatrix(between: sliceOfX, and: sliceOfY, where: predicate)
+
+        let changes = [ArrayIndexesChanges](DiffSequence(matrix: matrix))
+
+        let lowerBound = sliceOfX.startIndex
+        var inserts = changes
+            .filterInsertsIndexes(withOffset: lowerBound)
+            .sorted { $0 > $1 }
+
+        var deletes = changes
+            .filterDeletesIndexes(withOffset: lowerBound)
+            .sorted { $0 < $1 }
+
+        let moves = changes
+            .filterMovesIndexes(insertsIndexes: &inserts,
+                                deletesIndexes: &deletes,
+                                between: sliceOfX,
+                                and: sliceOfY,
+                                where: predicate)
+
+        let diff = Diff(inserts: inserts,
+                        deletes: deletes,
+                        moves: moves,
+                        insertsElement: inserts.flatMap { y[$0 - lowerBound] },
+                        deletesElement: deletes.flatMap { x[$0] },
+                        fromElements: x,
+                        toElements: y)
+
+        return diff
+    }
+
+    static func makeOptimalSlice(between x: [Element],
+                                 and y: [Element],
+                                 where predicate: Predicate) -> ArraySlice<Element> {
+        var startIndex = x.startIndex
+        var endIndex = x.endIndex
+
+        for (i, xElem) in x.lazy.enumerated() {
+            guard i < y.count else { break }
+
+            startIndex = i
+            let yElem = y[i]
+            if !predicate(xElem, yElem) {
+                break
+            }
+        }
+
+        for (i, xElem) in x.lazy.enumerated().reversed() {
+            guard i < y.count, startIndex < endIndex else { break }
+
+            endIndex = i + 1
+            let yElem = y[i]
+            if !predicate(xElem, yElem) {
+                break
+            }
+
+        }
+
+        return x[startIndex..<endIndex]
+    }
+
+    static func makeLCSMatrix(between x: ArraySlice<Element>,
+                              and y: ArraySlice<Element>,
+                              where predicate: Predicate) -> Matrix<Int> {
+        var matrix = Matrix(rows: x.count + 1, columns: y.count + 1, repeatedValue: 0)
+        for (i, xElem) in x.enumerated() {
             for (j, yElem) in y.enumerated() {
                 if predicate(xElem, yElem) {
                     matrix[i + 1, j + 1] = matrix[i, j] + 1
@@ -129,43 +261,7 @@ extension Array {
                 }
             }
         }
-
-        let changes = [ArrayIndexesChanges](DiffSequence(matrix: matrix))
-        var inserts: [Int] = changes.flatMap { change -> Int? in
-            guard case .inserts(let index) = change else { return nil }
-            return index + lowerBond
-        }.sorted { $0 > $1 }
-
-        var deletes: [Int] = changes.flatMap { change -> Int? in
-            guard case .deletes(let index) = change else { return nil }
-            return index + lowerBond
-        }.sorted { $0 < $1 }
-
-        var moves: [(Int, Int)] = []
-
-        var deleted: Int = 0
-        for (deleteIndex, deleteAtIndex) in deletes.enumerated() {
-            let elem = x[deleteAtIndex]
-            let temp = inserts.index { predicate(y[$0 - lowerBond], elem) }
-            guard let insertIndex = temp else { continue }
-
-            let insertAtIndex = inserts[insertIndex]
-            deletes.remove(at: deleteIndex - deleted)
-            inserts.remove(at: insertIndex)
-            let movement = (deleteAtIndex, insertAtIndex)
-            moves.append(movement)
-            deleted += 1
-        }
-
-        let diff = Diff(inserts: inserts,
-                        deletes: deletes,
-                        moves: moves,
-                        insertsElement: inserts.flatMap { y[$0 - lowerBond] },
-                        deletesElement: deletes.flatMap { x[$0] },
-                        fromElements: x,
-                        toElements: y)
-
-        return diff
+        return matrix
     }
 
 }
