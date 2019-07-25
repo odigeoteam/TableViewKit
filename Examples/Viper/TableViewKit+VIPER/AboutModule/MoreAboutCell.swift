59293c504ee12ca79fcb29eb388e2d9c082bbed2
Lorenzo Villarroel <lorenzo.villarroel@edreamsodigeo.com>
2019-07-25 16:23:57 +0200

    updated viper sample project to swift 5

--- Examples/Viper/TableViewKit+VIPER/AboutModule/MoreAboutCell.swift
+++ Examples/Viper/TableViewKit+VIPER/AboutModule/MoreAboutCell.swift
@@ -3,9 +3,9 @@ import TableViewKit
 
 class MoreAboutDrawer: CellDrawer {
 
-    static open var type = CellType.class(UITableViewCell.self)
+    static public var type = CellType.class(UITableViewCell.self)
 
-    static open func draw(_ cell: UITableViewCell, with item: MoreAboutItem) {
+    static public func draw(_ cell: UITableViewCell, with item: MoreAboutItem) {
         cell.accessoryType = .disclosureIndicator
         cell.textLabel?.text = item.title
     }
