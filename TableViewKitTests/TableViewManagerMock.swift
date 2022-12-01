98c724f7e6d2f24192931f0685a9da3baf8280ad
rmelian2014 <rmelian2014@gmail.com>
2022-12-01 15:19:04 +0100

    Centralise Z positioning logic of headers and footers (#48)
    * Centralise Z positioning logic of headers and footers
    
    * fixing small typo
    
    * Added new tests for Z position in header and footer
    
    Co-authored-by: Reinier Melian <reinier.melian@edreamsodigeo.com>

--- TableViewKitTests/TableViewManagerMock.swift
+++ TableViewKitTests/TableViewManagerMock.swift
@@ -4,11 +4,30 @@ final class TableViewManagerMock: TableViewManager {
     
     private(set) var zPositionForCellCallsCount = 0
     private(set) var zPositionForCellReceivedIndexPath: IndexPath?
+    private(set) var zPositionForHeaderCallsCount = 0
+    private(set) var zPositionForHeaderReceivedSection: Int?
+    private(set) var zPositionForFooterCallsCount = 0
+    private(set) var zPositionForFooterReceivedSection: Int?
     var zPositionForCellReturnValue: CGFloat?
+    var zPositionForHeaderReturnValue: CGFloat?
+    var zPositionForFooterReturnValue: CGFloat?
     
     override func zPositionForCell(at indexPath: IndexPath) -> CGFloat? {
         zPositionForCellCallsCount += 1
         zPositionForCellReceivedIndexPath = indexPath
         return zPositionForCellReturnValue
     }
+
+    override func zPositionForHeader(in section: Int) -> CGFloat? {
+        zPositionForHeaderCallsCount += 1
+        zPositionForHeaderReceivedSection = section
+        return zPositionForHeaderReturnValue
+    }
+
+    override func zPositionForFooter(in section: Int) -> CGFloat? {
+        zPositionForFooterCallsCount += 1
+        zPositionForFooterReceivedSection = section
+        return zPositionForFooterReturnValue
+    }
+
 }
